f84d4c1b471ce8931167335d6b28384cf04f6aed
Connor Findlay <findlaconn@ecs.vuw.ac.nz>
2015-02-10 21:32:14 +1300

    Initial version. This is a *very* basic implementation. Doubtless contains many bugs. When I have time, I want to improve the look of the thing, and remove the nned for continual refreshes.

---
+++ index.js
@@ -0,0 +1,41 @@
+//Application Stack
+var express = require('express');
+var WordGenerator = require('wordgenerator');
+var bodyParser = require('body-parser');
+//Variables
+var app = express();
+var generator = new WordGenerator();
+
+//Set up jade to render pages
+app.set('views', __dirname+'/views');
+app.set('view engine', 'jade');
+//Root
+app.get('/', function(req,res){
+    res.render('index');
+});
+
+//Set up POST formatting stuff
+app.use(bodyParser.urlencoded({extended: true}));
+//Form submitted
+app.post('/process-param', function(req,res) {
+    var len = req.body.length;
+    var sep = req.body.seperator;
+    var randomWords;
+    generator.generate({num : len, separator : sep},function(err, words){
+        randomWords = words;
+    });
+    console.log('randomWords is: %s', randomWords);
+    res.send(randomWords);
+});
+
+
+//Globals for listening server
+var host;
+var port;
+//Start server and listen!
+var server = app.listen(8888, '127.0.0.1', function() {
+    host = server.address().address;
+    port = server.address().port;
+
+    console.log('Listening at http://%s:%s', host, port);
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+Node-Name-Generator
+===================
+This is simple, configurable, name generator for
+GitHub repository naming. It is based off of
+(node-wordgenerator)[https://github.com/jesseditson/node-wordgenerator].
+
